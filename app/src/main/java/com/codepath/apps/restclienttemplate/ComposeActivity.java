18eed613b4170ea5fc64ee26af7a1029245cb181
Julia <julialn.sunni@gmail.com>
2019-11-07 20:54:28 -0500

    implemented ComposeActivity & tweet posting

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -0,0 +1,86 @@
+package com.codepath.apps.restclienttemplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcel;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.google.android.material.snackbar.Snackbar;
+
+import org.json.JSONException;
+import org.parceler.Parcels;
+
+import okhttp3.Headers;
+
+public class ComposeActivity extends AppCompatActivity {
+
+    private static final String TAG = "ComposeActivity";
+    public static final int MAX_TWEET_LENGTH = 140;
+
+    RelativeLayout composeLayout;
+    EditText edtTweet;
+    Button btnTweet;
+
+    TwitterClient client = TwitterApp.getRestClient(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose);
+
+        composeLayout = findViewById(R.id.composeLayout);
+        edtTweet = findViewById(R.id.edtTweet);
+        btnTweet = findViewById(R.id.btnTweet);
+
+        btnTweet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String tweetContent = edtTweet.getText().toString();
+                if (tweetContent.isEmpty()) {
+                    Snackbar.make(composeLayout, "Could not add empty tweet!", Snackbar.LENGTH_LONG).show();
+                    //Toast.makeText(ComposeActivity.this, "Could not add empty tweet!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                else if (tweetContent.length() > MAX_TWEET_LENGTH){
+                    Snackbar.make(composeLayout, "Too many characters!", Snackbar.LENGTH_LONG).show();
+                    return;
+                }
+                Snackbar.make(composeLayout, tweetContent, Snackbar.LENGTH_LONG).show();
+
+                // make API call to Twitter to publish tweet
+                client.publishTweet(tweetContent, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Headers headers, JSON json) {
+                        Log.i(TAG, "on publish success");
+                        try {
+                            Tweet tweet = Tweet.fromJson(json.jsonObject);
+                            Log.i(TAG, "Published tweet" + tweet.body);
+                            Intent intent = new Intent();
+                            intent.putExtra("tweet", Parcels.wrap(tweet));
+                            setResult(RESULT_OK, intent);
+                            finish();
+                        } catch (JSONException e) {
+                            Log.e(TAG, "json could not be parsed!");
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                        Log.e(TAG, "on publish failure " + throwable);
+                    }
+                });
+            }
+        });
+
+
+    }
+}
--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,12 +1,17 @@
 package com.codepath.apps.restclienttemplate;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -15,6 +20,7 @@ import com.github.scribejava.apis.TwitterApi;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +28,8 @@ import java.util.List;
 import okhttp3.Headers;
 
 public class TimelineActivity extends AppCompatActivity {
+
+    private final int REQUEST_CODE = 10;
 
     TwitterClient client;
     RecyclerView rvTweets;
@@ -63,6 +71,40 @@ public class TimelineActivity extends AppCompatActivity {
 
         populateHomeTimeline();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.compose) {
+            Toast.makeText(this,"Compose", Toast.LENGTH_SHORT).show();
+            // navigate to compose activity
+            Intent intent = new Intent(this, ComposeActivity.class);
+            startActivityForResult(intent, REQUEST_CODE);
+        }
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        Log.i("dfd", "first step");
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            // get data
+            Tweet tweet = Parcels.unwrap(data.getParcelableExtra("tweet"));
+            // update RecyclerView with tweet
+            // Modify data source of tweets
+            tweets.add(0,tweet);
+            // Notify adapter
+            adapter.notifyItemInserted(0);
+            rvTweets.smoothScrollToPosition(0);
+            Log.i("dfd", "smooth smooth");
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     private void populateHomeTimeline() {
